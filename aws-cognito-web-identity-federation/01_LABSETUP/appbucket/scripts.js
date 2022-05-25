a1b954212915387dacfa6eaebcdea56d096d9a42
Adrian Cantrill <adrian@cantrill.io>
2022-05-26 08:54:35 +1000

    updates to webIDF

--- aws-cognito-web-identity-federation/01_LABSETUP/appbucket/scripts.js
+++ aws-cognito-web-identity-federation/01_LABSETUP/appbucket/scripts.js
@@ -1,43 +1,42 @@
-function onSignIn(googleUser) {
-  // profile info
-  var profile = googleUser.getBasicProfile();
-  console.log('Full Name: ' + profile.getName());
-  console.log('Email: ' + profile.getEmail());
-  // auth response
-  console.log('Google authentication response:');
-  var authResponse = googleUser.getAuthResponse()
-  console.log(authResponse);
-  var id_token = authResponse.id_token;
-  console.log('JWT token (encrypted): ' + id_token);
-  console.log('JWT token (decrypted):');
-  console.log(parseJwt(id_token));
-  signInCallback(authResponse);
+
+function onSignIn(googleToken) {
+  // Google have OK'd the sign-in
+  // pass the token into our web app
+  credentialExchange(googleToken);
 }
 
-// A utility function to create HTML.
-function getHtml(template) {
-  return template.join('\n');
-}
-
-function signInCallback(authResult) {
-  if (authResult['access_token']) {
+function credentialExchange(googleToken) {
+  // Create a decoded version of the token so we can print things out
+  console.log("Creating decoded token...");
+  const googleTokenDecoded = parseJwt(googleToken.credential);
+  
+  // Output some details onto the browser console to show the token working
+  console.log("ID: " + googleTokenDecoded.sub);
+  console.log('Full Name: ' + googleTokenDecoded.name);
+  console.log("Email: " + googleTokenDecoded.email);
+  
+  if (googleTokenDecoded['sub']) {
     
-    // adding google access token to Cognito credentials login map
+    // We can't access anything in AWS with a google token...
+    // ... so we need to exchange it using Cognito for AWS credentials
+    console.log("Exchanging Google Token for AWS credentials...");
     AWS.config.region = 'us-east-1'; 
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', // MAKE SURE YOU REPLACE THIS
+      IdentityPoolId: 'REPLACE_ME_COGNITO_IDENTITY_POOL_ID', // MAKE SURE YOU REPLACE THIS
       Logins: {
-        'accounts.google.com': authResult['id_token']
+        'accounts.google.com': googleToken.credential
       }
     });
 
-    // obtain credentials
+    // Now lets obtain the credentials we just swapped
     AWS.config.credentials.get(function(err) {
       if (!err) {
-        console.log('Cognito Identity Id: ' + AWS.config.credentials.identityId);
-        // test aws
-        testAWS();
+        console.log('Exchanged to Cognito Identity Id: ' + AWS.config.credentials.identityId);
+        // if we are here, things are working as they should...
+        // ... now lets call a function to access images, generate signed URL's and display
+        accessImages();
       } else {
+        // if we are here, bad things have happened, so we should error.
         document.getElementById('output').innerHTML = "<b>YOU ARE NOT AUTHORISED TO QUERY AWS!</b>";
         console.log('ERROR: ' + err);
       }
@@ -48,22 +47,18 @@ function signInCallback(authResult) {
   }
 }
 
-function parseJwt(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
-  var plain_token = JSON.parse(window.atob(base64));
-  return plain_token;
-};
-
-function testAWS() {
+function accessImages() {
   
+  // Using the temp AWS Credentials, lets connect to S3
+  console.log("Creating Session to S3...");
   var s3 = new AWS.S3();
   var params = {
-    Bucket: "REPLACE_ME_PRIVATE_PATCHES_BUCKET" // MAKE SURE YOU REPLACE THIS
+    Bucket: "REPLACE_ME_NAME_OF_PATCHES_PRIVATE_BUCKET" // MAKE SURE YOU REPLACE THIS
   }; 
 
+  // If we are here, things are going well, lets list all of the objects in the bucket
   s3.listObjects(params, function(err, data) {
-
+    console.log("Listing objects in patchesprivate bucket...");
     if (err) {
       document.getElementById('output').innerHTML = "<b>YOU ARE NOT AUTHORISED TO QUERY AWS!</b>";
       console.log(err, err.stack);
@@ -72,9 +67,12 @@ function testAWS() {
       console.log(data);
       var href = this.request.httpRequest.endpoint.href;
       var bucketUrl = href + data.Name + '/';
+      
+      // for all of the images in the bucket, we need to generate a signedURL for the object
       var photos = data.Contents.map(function(photo) {
         var photoKey = photo.Key;
-
+        
+        console.log("Generating signedURL for : " + photoKey);
         var url = s3.getSignedUrl ('getObject', {
           Bucket: data.Name,
           Key: photoKey
@@ -95,19 +93,24 @@ function testAWS() {
         ]);
       });
 
+      // let's take those signedURL's, create a HTML page, and display it in the web browser
       var htmlTemplate = [ '<div>',   getHtml(photos), '</div>']
+      console.log("Creating and returning html...")
       document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
     }    
 
   });
 }
 
-function onSignOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-  AWS.config.credentials.clearCachedId();
-  document.getElementById('output').innerHTML = "";
-  document.getElementById('viewer').innerHTML = "";
+// A utility function to create HTML.
+function getHtml(template) {
+  return template.join('\n');
 }
+
+// A utility function to decode the google token
+function parseJwt(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace('-', '+').replace('_', '/');
+  var plain_token = JSON.parse(window.atob(base64));
+  return plain_token;
+};
